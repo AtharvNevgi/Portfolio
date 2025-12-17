383b115eece79977648eec66f5a0f35eab86f9ba
Atharva Nevgi <atharva.nevgi579@gmail.com>
2025-12-17 22:18:43 +0530

    changed db to atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config({quiet:true})
 
-mongoose.connect("mongodb://localhost:27017/Portfolio")
+// mongoose.connect("mongodb://localhost:27017/Portfolio")
+mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     console.log("Database Connected");
 })
